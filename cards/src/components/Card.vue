8efd1e141538b315625cf746bb0625ab67f9d761
fowler74 <fowler74@icloud.com>
2019-07-14 12:46:05 -0400

    adding vuejs files

---
+++ cards/src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+  	{{suit}} - {{value}}
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Card',
+  props: ['suit', 'value'],
+  data () {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ cards/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Deck from '@/components/Deck'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Deck',
+      component: Deck
+    }
+  ]
+})
